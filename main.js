0f764ee64b417b67958cd6bb701d1e612cc28e99
ValiantHail <83453840+ValiantHail@users.noreply.github.com>
2021-05-03 19:50:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var x="The Egyptian pyramids are ancient pyramid-shaped masonry structures located in Egypt. As of November 2008, sources cite either 118 or 138 as the number of identified Egyptian pyramids.[1][2] Most were built as tombs for the country's pharaohs and their consorts during the Old and Middle Kingdom periods.[3][4][5]
+
+The earliest known Egyptian pyramids are found at Saqqara, northwest of Memphis, although at least one step-pyramid-like structure has been found at Saqqara, dating to the First Dynasty: Mastaba 3808, which has been attributed to the reign of Pharaoh Anedjib, with inscriptions, and other archaeological remains of the period, suggesting there may have been others.[6] The otherwise earliest among these is the Pyramid of Djoser built c. 2630–2610 BCE during the Third Dynasty.[7] This pyramid and its surrounding complex are generally considered to be the world's oldest monumental structures constructed of dressed masonry.[8]
+
+The most famous Egyptian pyramids are those found at Giza, on the outskirts of Cairo. Several of the Giza pyramids are counted among the largest structures ever built.[9] The Pyramid of Khufu is the largest Egyptian pyramid. It is the only one of the Seven Wonders of the Ancient World still in existence."
+function info() {
+    window.alert(x)
+}
